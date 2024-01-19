3950c1facf6530c2a1ed592cdb0bbb210a7b60ec
Abdallah <abd3allah9a@gmail.com>
2024-01-20 00:20:28 +0200

    Temperature-Convertor

---
+++ Temperature-Convertor/Assignment 4/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Assignment_4
+{
+    partial class Form1
+    {
+  
+        private void InitializeComponent()
+        {
+            temp = new Label();
+            temp.Size = new System.Drawing.Size(100, 20);
+            temp.Text = "Temperature";
+            temp.Location = new System.Drawing.Point(50, 50);
+            temp.BackColor = Color.Transparent;
+            Controls.Add(temp);
+
+            converfrom = new Label();
+            converfrom.Size = new System.Drawing.Size(100, 20);
+            converfrom.Text = "Convert from";
+            converfrom.Location = new System.Drawing.Point(50, 100);
+            converfrom.BackColor = Color.Transparent;
+            Controls.Add(converfrom);
+
+            converto = new Label();
+            converto.Size = new System.Drawing.Size(100, 20);
+            converto.Text = "Convert to";
+            converto.Location = new System.Drawing.Point(50, 150);
+            converto.BackColor = Color.Transparent;
+            Controls.Add(converto);
+
+            result = new Label();
+            result.Size = new System.Drawing.Size(100, 20);
+            result.Text = "Result";
+            result.Location = new System.Drawing.Point(50, 200);
+            result.BackColor = Color.Transparent;
+            Controls.Add(result);
+
+            temptext = new TextBox();
+            temptext.Size = new Size(150, 25);
+            temptext.Location = new System.Drawing.Point(170, 50);
+            Controls.Add(temptext);
+
+            resulttext = new TextBox();
+            resulttext.Size = new Size(150, 25);
+            resulttext.Location = new System.Drawing.Point(170, 200);
+            Controls.Add(resulttext);
+
+            cel = new RadioButton();
+            cel.Size = new System.Drawing.Size(100, 20);
+            cel.Text = "Celsius";
+            cel.Location = new System.Drawing.Point(170, 150);
+            cel.BackColor = Color.Transparent;
+            Controls.Add(cel);
+
+            fah = new RadioButton();
+            fah.Size = new System.Drawing.Size(100, 20);
+            fah.Text = "Fahrenheit";
+            fah.Location = new System.Drawing.Point(300, 150);
+            fah.BackColor = Color.Transparent;
+            Controls.Add(fah);
+
+            conver = new Button();
+            conver.Size = new Size(120, 30);
+            conver.Location = new System.Drawing.Point(180, 250);
+            conver.Text = "Convert";
+            conver.Click += Convert_Click;
+            Controls.Add(conver);
+
+            choiceBox = new ComboBox();
+            choiceBox.Size = new System.Drawing.Size(150, 20);
+            choiceBox.Location = new System.Drawing.Point(170, 100);
+            choiceBox.Items.Add("Fahrenheit");
+            choiceBox.Items.Add("Celsius");
+            choiceBox.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            choiceBox.AutoCompleteSource = AutoCompleteSource.ListItems;
+            Controls.Add(choiceBox);
+
+
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 300);
+            this.Name = "Form1";
+            this.Text = "Temperature convertor";
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.AutoSize = false;
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
+        }
+        private Label temp;
+        private Label converfrom;
+        private Label converto;
+        private Label result;
+
+        private TextBox temptext;
+        private TextBox resulttext;
+        private ComboBox choiceBox;
+
+        private RadioButton cel;
+        private RadioButton fah;
+
+        private Button conver;
+
+
+
+    }
+}
+
---
+++ Temperature-Convertor/Assignment 4/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Convert_Click(object sender, EventArgs e)
+        {
+            if (temptext == null)
+            {
+                MessageBox.Show("Please enter the temperature.");
+                return;
+            }
+            if (!(cel.Checked || fah.Checked))
+            {
+                MessageBox.Show("Please select the temperature type (Celsius or Fahrenheit).");
+                return;
+            }
+            if (choiceBox.SelectedItem == null)
+            {
+                MessageBox.Show("Please select the conversion type (Celsius or Fahrenheit).");
+                return;
+            }
+
+
+            else
+            {
+                if(choiceBox.SelectedItem == "Fahrenheit" && fah.Checked) 
+                {
+                    resulttext.Text = temptext.Text.ToString();
+                }
+                else if (choiceBox.SelectedItem == "Celsius" && cel.Checked)
+                {
+                    resulttext.Text = temptext.Text.ToString();
+                }
+                else if(choiceBox.SelectedItem == "Fahrenheit" && cel.Checked)
+                {
+                    double res = (double.Parse(temptext.Text) - 32) * 5 / 9;
+                    resulttext.Text = res.ToString();
+                }
+                else
+                {
+                    double res = (double.Parse(temptext.Text) * 9 / 5) + 32;
+                    resulttext.Text = res.ToString();
+                }
+            }
+        }
+
+    }
+}
